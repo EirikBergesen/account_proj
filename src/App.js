de2384d12f649748dcc8936fbb893bd3b534d134
EirikBergesen <eirikbr@yahoo.com>
2023-11-14 18:39:42 +0100

    Extended tut

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+
+
+function FilterableProductTable({ products }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
     </div>
   );
 }
 
-export default App;
+
+function SearchBar({database}) {
+  return (
+    <form>
+    <input type="text" placeholder="Search..." />
+    <label>
+      <input type="checkbox" />
+      {' '}
+      Only show products in stock
+    </label>
+  </form>
+  );
+}
+
+
+function ProductTable( {products} ) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />
+      );
+    }
+    rows.push(
+      <ProductRow
+      product={product}
+      key={product.name}
+      />
+    );
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+
+function ProductCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">
+        {category}
+      </th>
+    </tr>
+  );
+}
+
+
+function ProductRow( {product} ) {
+  const name = product.stocked ? product.name :
+    <span style={{color: 'red'}}>
+      {product.name}
+    </span>;
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+
+export default function App() {
+  // import data from json
+  const fruit_vendor = require('./fruit_vendor.json');
+
+  return (
+  <FilterableProductTable products={fruit_vendor} />
+  );
+}
